0d6bd751849fecc699cf715efac2424957173dc9
DZHURA0o0 <152070795+DZHURA0o0@users.noreply.github.com>
2024-05-11 11:00:03 +0300

    бля
    бля

---
+++ CourseWorkWeb/Controllers/CatalogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourseWorkWeb.Controllers
+{
+    public class CatalogController : Controller
+    {
+        public IActionResult Catolog()
+        {
+            return View();
+        }
+    }
+}
--- CourseWorkWeb/Controllers/HomeController.cs
+++ CourseWorkWeb/Controllers/HomeController.cs
@@ -28,5 +28,9 @@ namespace CourseWorkWeb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Catalog()
+        {
+            return Redirect("~/Catalog");
+        }
     }
 }
