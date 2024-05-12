27f8dd15b1a13b27954a5c1bcf57fc3cea88cc92
DZHURA0o0 <152070795+DZHURA0o0@users.noreply.github.com>
2024-05-12 17:20:32 +0300

    p
    o

--- CourseWorkWeb/Controllers/CatalogController.cs
+++ CourseWorkWeb/Controllers/CatalogController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CourseWorkWeb.Core.CQRS.Medicines.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CourseWorkWeb.Controllers
 {
-    public class CatalogController : Controller
+    public class MadicineController(ISender sender):Controller
     {
+        private readonly ISender _sender = sender;
         public IActionResult Index()
         {
             return View();
         }
+        public async Task<ActionResult> GetAllMedicines()
+        {
+            var medicine = await _sender.Send(new GetMedicinesQuery());
+            return View(medicine);
+            
+        }
+        
     }
 }
