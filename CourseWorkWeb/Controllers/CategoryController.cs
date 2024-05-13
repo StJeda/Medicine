05871846d70e712f3e7dee605d6aff28832e1bdb
DZHURA0o0 <152070795+DZHURA0o0@users.noreply.github.com>
2024-05-13 21:04:06 +0300

    ds
    ds

--- CourseWorkWeb/Controllers/CategoryController.cs
+++ CourseWorkWeb/Controllers/CategoryController.cs
@@ -11,17 +11,15 @@ namespace CourseWorkWeb.Controllers
        private readonly ISender _sender = sender;
 
         public async Task<IActionResult> Index(string word)
-        {
-            if (word == "Medicine")
-            {
-                var medicines = await _sender.Send(new GetMedicinesQuery());
-                ViewBag.Word = word;
-                return View();
+        { 
+            var medicines = await _sender.Send(new GetMedicinesQuery());
+             List<Medicine> filteredMedicines = new List<Medicine>();
+
+            foreach(var item in medicines){
+               if(item.Type==word){filteredMedicines.Add(item);} 
             }
-            else
-            {
-                return BadRequest(); // Возвращаем ошибку, если параметр word не равен "Medicine"
-            }
+            ViewBag.Category=word;
+            return View(filteredMedicines);
         }
     }
 }
