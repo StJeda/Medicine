a97ae16c8f96de663af132ad05161aae089c961b
DZHURA0o0 <152070795+DZHURA0o0@users.noreply.github.com>
2024-05-13 19:39:48 +0300

    ен
    н

---
+++ CourseWorkWeb/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CourseWorkWeb.Core.CQRS.Medicines.Queries;
+using CourseWorkWeb.Models.Entity.Medicines;
+using MediatR;
+
+
+namespace CourseWorkWeb.Controllers
+{
+    public class CategoryController(ISender sender) : Controller
+    {
+       private readonly ISender _sender = sender;
+
+        public async Task<IActionResult> Index(string word)
+        {
+            if (word == "Medicine")
+            {
+                var medicines = await _sender.Send(new GetMedicinesQuery());
+                ViewBag.Word = word;
+                return View();
+            }
+            else
+            {
+                return BadRequest(); // Возвращаем ошибку, если параметр word не равен "Medicine"
+            }
+        }
+    }
+}
