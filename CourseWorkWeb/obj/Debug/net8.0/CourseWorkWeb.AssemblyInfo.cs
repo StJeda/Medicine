fd8f5aa871afe153013dd3ce70b816da601c5753
DZHURA0o0 <152070795+DZHURA0o0@users.noreply.github.com>
2024-05-21 17:56:33 +0300

    ывапр
    впы

--- CourseWorkWeb/obj/Debug/net8.0/CourseWorkWeb.AssemblyInfo.cs
+++ CourseWorkWeb/obj/Debug/net8.0/CourseWorkWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseWorkWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81708821a850fbba95c57bb7448dea406cd4b6e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+748d7abb491df7b4219c021e1cc87f1639fb98c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseWorkWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseWorkWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
